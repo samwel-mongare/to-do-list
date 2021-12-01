6e6fc378e69bb923a9abae0cd364b4c0faf532ca
Mong'are <Mongaresams@gmail.com>
2021-12-01 13:09:13 +0300

    Fix linters errors

--- src/index.js
+++ src/index.js
@@ -1,44 +1,44 @@
-import _ from 'lodash';
 import './style.css';
-const todays_list = document.getElementById('todays-list');
+
+const todaysList = document.getElementById('todays-list');
 
 const tasksTodo = [
-    {
-        description: 'Prepare breakfast',
-        completed: false,
-        index: 1,
-    },
-    {
-        description: 'Prepare Lunch',
-        completed: false,
-        index: 2,
-    },
-    {
-        description: 'Pick the kids',
-        completed: false,
-        index: 3,
-    },
-    {
-        description: 'Go shopping',
-        completed: false,
-        index: 4,
-    },
+  {
+    description: 'Prepare breakfast',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Prepare Lunch',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Pick the kids',
+    completed: false,
+    index: 3,
+  },
+  {
+    description: 'Go shopping',
+    completed: false,
+    index: 4,
+  },
 ];
 
-window.addEventListener('load', createList());
-
 function createList() {
-    [...tasksTodo]
+  [...tasksTodo]
     .forEach((task, index) => {
-        const listItem = document.createElement('li');
-        listItem.setAttribute('data-item', index);
-        listItem.innerHTML = `
+      const listItem = document.createElement('li');
+      listItem.setAttribute('data-item', index);
+      listItem.innerHTML = `
         <div class= "listy">
         <input type="checkbox" id="${index}" name="task1" value="task1">
         <label for="${index}">${task.description}</label>
         </div>
             <span>&#8942;</span>
         `;
-    todays_list.appendChild(listItem);
+      todaysList.appendChild(listItem);
     });
 }
+
+window.addEventListener('load', createList());
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
@@ -7,22 +8,22 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  
+
   devServer: {
     static: './dist',
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-        template: './src/index.html',
-          }),
-      ],
-      module: {
-       rules: [
-          {
-            test: /\.css$/i,
-           use: ['style-loader', 'css-loader'],
-       },
-       ],
-    },
+      template: './src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
