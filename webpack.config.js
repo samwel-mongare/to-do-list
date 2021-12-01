7d73996efd9cf741ba33aef17de226ba7f598982
Mong'are <Mongaresams@gmail.com>
2021-12-01 13:41:52 +0300

    Fix linters errors

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
