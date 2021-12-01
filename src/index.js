46085397ec510f928e0769ee8edc4a487b9b8c02
Mong'are <Mongaresams@gmail.com>
2021-12-01 12:40:03 +0300

    Add to-do list structure into the repo

--- src/index.js
+++ src/index.js
@@ -1,13 +1,44 @@
 import _ from 'lodash';
 import './style.css';
+const todays_list = document.getElementById('todays-list');
 
- function component() {
-   const element = document.createElement('div');
+const tasksTodo = [
+    {
+        description: 'Prepare breakfast',
+        completed: false,
+        index: 1,
+    },
+    {
+        description: 'Prepare Lunch',
+        completed: false,
+        index: 2,
+    },
+    {
+        description: 'Pick the kids',
+        completed: false,
+        index: 3,
+    },
+    {
+        description: 'Go shopping',
+        completed: false,
+        index: 4,
+    },
+];
 
-  // Lodash, now imported by this script
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+window.addEventListener('load', createList());
 
-   return element;
- }
-
- document.body.appendChild(component());
+function createList() {
+    [...tasksTodo]
+    .forEach((task, index) => {
+        const listItem = document.createElement('li');
+        listItem.setAttribute('data-item', index);
+        listItem.innerHTML = `
+        <div class= "listy">
+        <input type="checkbox" id="${index}" name="task1" value="task1">
+        <label for="${index}">${task.description}</label>
+        </div>
+            <span>&#8942;</span>
+        `;
+    todays_list.appendChild(listItem);
+    });
+}
